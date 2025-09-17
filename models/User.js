1be19df461d06b1beec1f7adaaadcb118763fba9
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-09-17 17:41:40 +0100

    Create User.js
    Users password hashed

---
+++ models/User.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      postalCode: String,
+    },
+    phone: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//  Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//  Compare entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
