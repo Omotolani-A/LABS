786e43e7bbf9206aa5890eb82d76c019768f7645
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-09-17 19:21:18 +0100

    Update index.js
    Mounted routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -12,5 +13,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
