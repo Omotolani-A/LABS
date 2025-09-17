09806d8596145a8aae50a9c44668b62b98572956
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-09-17 19:15:11 +0100

    Create authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+// routes/authRoutes.js
+import express from "express";
+import { registerUser, loginUser, getUserProfile } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Public routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+// Private route (requires JWT)
+router.get("/profile", protect, getUserProfile);
+
+export default router;
