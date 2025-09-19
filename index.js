b66d1ed2f84056e95e521898c92a9684f4edb593
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-09-20 00:26:28 +0100

    Update index.js
    Added product routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+
+app.use("/api/product", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
