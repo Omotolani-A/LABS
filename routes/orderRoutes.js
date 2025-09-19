4991ba139d17fc0032f5de7f6f65c436042b42fa
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-09-20 00:42:40 +0100

    Create orderRoutes.js

---
+++ routes/orderRoutes.js
@@ -0,0 +1,17 @@
+// routes/orderRoutes.js
+import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import { admin } from "../middleware/adminMiddleware.js";
+import { placeOrder, getMyOrders, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
+
+const router = express.Router();
+
+// Customer routes
+router.post("/", protect, placeOrder);
+router.get("/my", protect, getMyOrders);
+
+// Admin routes
+router.get("/", protect, admin, getAllOrders);
+router.put("/:id", protect, admin, updateOrderStatus);
+
+export default router;
